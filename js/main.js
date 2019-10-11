1da5d1556eb32ba993e2758179c36f9e330f9fa1
cmonico <46007796+cmonico@users.noreply.github.com>
2019-10-11 16:52:31 -0400

    Built Out Homepage
    Continued making homepage. Nav is fully responsive with jquery for hamburger menu. Footer is mostly built out with some minor adjustments. Next, the main static content needs to be made responsive and other pages can be started after that.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  $("navbar-toggler").click(function(){
-    $("this").hide();
+  $(".hamburger_menu").click(function(){
+    $(".mobile_nav_text").slideToggle("fast");
+    $(".main_nav_phone").toggleClass('main_nav_phone_toggle');
+
   });
 });
