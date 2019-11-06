de1cdd0d4fd4284ac1497bb87414d4b801f27df8
cmonico <46007796+cmonico@users.noreply.github.com>
2019-11-06 16:35:00 -0500

    Contact Modal
    Added Contact Form Modal (Which needs to become a disappearing form) & adjusted nav to have anchors.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ $(document).ready(function(){
   $(".hamburger_menu").click(function(){
     $(".mobile_nav_text").slideToggle("fast");
     $(".main_nav_phone").toggleClass('main_nav_phone_toggle');
+  });
 
+  $(".aboutjq").click(function(){
+    $(".mobile_nav_text").hide("fast");
+    $(".main_nav_phone").toggleClass('main_nav_phone_toggle');
+  });
+
+  /* Modal State */
+  $(".submit-button").click(function(){
+    $(".contact-modal").css({"display": "block"});
+  });
+
+  $(".close_modal").click(function(){ /* Closes Modal By X */
+    $(".contact-modal").css({"display": "none"});
   });
 });
